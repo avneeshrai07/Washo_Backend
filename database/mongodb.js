612cb4e99dbaac596dc730e763c726fb85c64e78
avneeshrai07 <lcb2021026@iiitl.ac.in>
2024-12-11 14:39:42 +0530

    audit fixes

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,5 @@
-// mongodb.js
-
 const connectToDatabase = require('./dbConnection');
+
 const SignUpModel = require('./userModel');
 const paymentModel = require('./paymentModel');
 const messageModel = require('./messageModel');
--- routes/pushMessageRoute.js
+++ routes/pushMessageRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pushMessageController = require('../controllers/pushMessageController');
-console.log("message Route Accesed")
 router.post('/message', pushMessageController);
 
 module.exports = router;
